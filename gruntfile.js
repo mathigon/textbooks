af7ce57b1078c1c2e4bc0017039b07eedf4e9625
Philipp Legner <philipp@mathigon.org>
2019-05-07 10:20:54 +0100

    Add Chinese translations for Divisibility and primes

--- gruntfile.js
+++ gruntfile.js
@@ -71,7 +71,7 @@ grunt.initConfig({
   },
 
   textbooks: {
-    options: {languages: ['en', 'ru', 'vn'], cache: true},
+    options: {languages: ['en', 'ru', 'vn', 'cn'], cache: true},
     app: {
       files: [{
         expand: true,
