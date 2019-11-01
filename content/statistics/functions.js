f7f5f01a3cafcc8006afc6f13e56e52aaca77dfb
Philipp Legner <philipp@mathigon.org>
2019-11-01 08:15:07 +0000

    Fix probability bugs

--- content/statistics/functions.js
+++ content/statistics/functions.js
@@ -4,8 +4,10 @@
 // =============================================================================
 
 
-import { roundTo } from '@mathigon/fermat';
-import { rotateDisk } from '../shared/components/disk';
+import {clamp, last, square, total} from '@mathigon/core';
+import {random, roundTo} from '@mathigon/fermat';
+import {rotateDisk} from '../shared/components/disk';
+
 
 export function roulette($step) {
   const $wheels = $step.$$('.wheel');
@@ -42,5 +44,50 @@ export function roulette($step) {
       const y = -ballRadius * Math.cos(ballAngle);
       $ball.translate(x, y);
     }
+  });
+}
+
+export function randomSequence($section) {
+
+  function generatePossibilities(len) {
+    if (len <= 1) return [['R', 'B']];
+    let prev = generatePossibilities(len - 1);
+    prev.push(last(prev).map(x => x + 'R').concat(last(prev).map(x => x + 'B')));
+    return prev;
+  }
+
+  function findCount(str, substr) {
+    let count = 0;
+    for (let i=0; i<=str.length - substr.length; ++i) {
+      count += str.slice(i).startsWith(substr);
+    }
+    return count;
+  }
+
+  function compute(str) {
+    let max = clamp(Math.floor(str.length / 2), 1, 8);
+    let poss = generatePossibilities(max);
+    let result = 1;
+
+    for (let i = 2; i <= max; ++i) {
+      let values = poss[i - 1];
+      let count = values.length;
+
+      let observed = values.map(v => findCount(str, v));
+      let sum = total(observed);
+
+      let chi = total(observed.map(o => square(o - sum / count) / sum * count));
+      let deg = count - 1;
+
+      result = Math.min(result, random.chiCDF(chi, deg));
+    }
+
+    return clamp(result, 0, 1);
+  }
+
+  let $score = $section.$('.score');
+  $section.$('input').change(str => {
+    if (str.length > 7) $section.score('random');
+    $score.text = Math.round(compute(str.toUpperCase()) * 100);
   });
 }
