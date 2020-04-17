65cfd8baa870ff59f586ea83d3d5185a1839abcc
Kevin DeLand <kevin.deland@gmail.com>
2020-04-17 11:36:57 -0400

    Cleaned imports, comments, console.log

--- content/testing/functions.ts
+++ content/testing/functions.ts
@@ -4,26 +4,25 @@
 // =============================================================================
 
 // nothing to see here, folks
-import {Obj, Color} from '@mathigon/core';
-import {SVGView, animate, InputView, AnimationResponse, ElementView, $, observable, SVGBaseView} from '@mathigon/boost';
+import {Obj} from '@mathigon/core';
+import {SVGView, animate, InputView, AnimationResponse, ElementView, observable} from '@mathigon/boost';
 import {Step, Gameplay, Slideshow, Slider} from '../shared/types';
 import { Point, Random, SimplePoint } from '@mathigon/fermat';
-
-import './components/barcode';
-import { Barcode } from './components/barcode';
-
-
-const MORSE = {
-    '._': 'a',
-    '_...': 'b',
-    '_._.': 'c'
-    // TODO_MORSE to be continued
-}
 
 // TODO_MORSE_1 insert code table
 // TODO_MORSE_2 translation
 
+/**
+ * Varius morse code experiments
+ */
 export function morseDemo($section: Step) {
+
+    const MORSE = {
+        '._': 'a',
+        '_...': 'b',
+        '_._.': 'c'
+        // TODO_MORSE to be continued
+    }
 
     const $dot: ElementView = $section.$('.dot')!;
     const $dash: ElementView = $section.$('.dash')!;
@@ -69,8 +68,7 @@ export function morseDemo($section: Step) {
     $translate.on('click', () => {
         if(!typed) return;
         let translateme = $typeout.text;
-        console.log('translate!!');
-
+        // TODO: to be continued...
     });
 }
 
@@ -89,8 +87,6 @@ export function bracket($step: Step) {
     let lastStep = 0;
 
     const $slider = $step.$('x-slider.bracket') as Slider;
-    console.log($slider);
-
     const $rounds = $step.$$('g');
     const count = $rounds.length;
 
@@ -126,10 +122,11 @@ export function bracket($step: Step) {
     move(lastStep);
 }
 
+/** 
+ * Animating a telegraph SVG
+ */
 export function telegraph($section: Step) {
-    console.log("telegraph");
     const $arm = $section.$('g#arm') as SVGView;
-    console.log($arm);
 
     let open = false;
 
@@ -148,9 +145,10 @@ export function telegraph($section: Step) {
 
 }
 
+/**
+ * Animating a transistor
+ */
 export function transistor($section: Step) {
-
-    console.log('transistor2');
 
     const $pathOn = $section.$('#path_on') as SVGView;
     const $pathOff = $section.$('#path_off') as SVGView;
@@ -210,6 +208,9 @@ export function transistor($section: Step) {
     }
 }
 
+/**
+ * Slideshow for animating decimal to binary.
+ */
 export function dec2bin($section: Step) {
     const $slideshow = $section.$('x-slideshow') as Slideshow;
     
@@ -363,7 +364,6 @@ export function dec2bin($section: Step) {
         // $block8 disappears ($digitBlocks[digitIndex].hide(); )
         // $blockN goes back to its current size + 8
         const newN = digits[digitIndex] + N;
-        console.log(newN);
 
         setTimeout(() => {
             $digitBlocks[digitIndex].hide();
@@ -503,9 +503,11 @@ export function dec2bin($section: Step) {
     });
 }
 
+/**
+ * A table and text input for creating emoji-based codes
+ */
 export function emojiCodes($section: Step) {
 
-    console.log("emojiBoard");
     // 1. set up code data
     const hearts = ['💙', '💚', '💛', '💜', '🧡', '🖤'];
     const fruit = ['🍏', '🍎', '🍐', '🍊', '🍋', '🍌', '🍉', '🍇', '🍓', '🍈'];
@@ -532,8 +534,6 @@ export function emojiCodes($section: Step) {
     $section.$$('.books')!.forEach(hideAll);
     const $typeout: ElementView = $section.$('.typeout')!;
     let typed: boolean = false;
-
-    console.log($hearts.length, $sports.length, $fruit.length);
 
     const clickFarm = (map: string[]) => 
         ($f: ElementView, i: number) => {
@@ -608,12 +608,9 @@ export function emojiCodes($section: Step) {
     
             let charIndex = index1*5 + index2;
             // this one doesn't have "x"
-            console.log(charIndex);
             if (charIndex === 23 || charIndex === 24) charIndex++;
             
             translated += String.fromCharCode(97 + charIndex);
-            
-            console.log(translated);
         }
         $section.$('.output')!.text = translated;
 
@@ -622,174 +619,118 @@ export function emojiCodes($section: Step) {
     const $translate = $section.$('.translate')!;
     $translate.on('click', () => {
         let translateme = $typeout.text;
-        console.log(translateme);
         translateFun(translateme);
 
-
-    });
-
-    return; 
-    
-    console.log('emoji board');
-    const $textarea = $section.$('textarea')!;
-    console.log($textarea);
-
-    const $button = $section.$('button')!;
-
-    $textarea.onKeyDown('a', () => {
-        console.log('a');
-    });
-    let x = 0;
-
-    let hasTyped = false;
-
-    let ff = (e:any) => {
-        
-        (e as KeyboardEvent).stopImmediatePropagation();
-        switch((e as KeyboardEvent).key) {
-            case 'a': console.log('apple'); break;
-            case 'b': console.log('bruins'); break;
-            case 'c': console.log('coffee'); break;
-        }
-        console.log((e as KeyboardEvent));
-        console.log((e as KeyboardEvent).key);
-        
-        if (!hasTyped) {
-          //  $textarea.text = '';
-          //  hasTyped = true;
-        }
-        //$textarea.text += '' + e.key + hearts[2];
-        //$textarea._el.innerHTML = $textarea.text;
-
-        console.log($textarea.text);
-        console.log($textarea.textStr);
-
-    };
-
-    $button.on('click', (e) => {
-        console.log('click');
-        $textarea.text += 'x';
-    });
-
-//    $textarea._el.removeEventListener('keydown');
-    $textarea._el.addEventListener('keydown', ff, {once: false});
-
-    // EMOJI: this can't handle letters, numbers, anything except what's defined
-//    $textarea.onKeyDown('')
-    $textarea.onKeyDown('enter', () => {
-        console.log('enter');
     });
 }
 
-// hardcoded data... could it be simpler?
-const vertices: Obj<string[]> = {
-    "x": ["LineA", "Line0", "Line10", "Line16", "Line19", "Line22", "Line28", "Line30",
-"Line34", "Line41", "Line42", "Line44", "Line52", "Line55"],
-    "v0": ["LineA", "Line8", "Line15"],
-    "v1": ["Line8", "LineB", "Line2"],
-    "v2": ["Line0", "Line2", "Line1"],
-    "v3": ["Line10", "Line15", "Line27"],
-    "v4": ["Line27", "Line28", "Line9"],
-    "v5": ["Line26", "Line9", "Line30"],
-    "v6": ["LineB", "Line17", "Line24"],
-    "v7": ["Line1", "Line19", "Line3"],
-    "v8": ["Line3", "Line20", "Line21"],
-    "v9": ["Line21", "Line18", "Line23"],
-    "v10": ["Line23", "Line38", "Line4"],
-    "v11": ["Line38", "Line39", "Line43"],
-    "v12": ["Line39", "Line40", "Line22"],
-    "v13": ["Line40", "Line41", "Line42"],
-    "v14": ["Line16", "Line17", "Line18"],
-    "v15": ["Line24", "Line25", "Line36"],
-    "v16": ["Line31", "Line32", "Line35"],
-    "v17": ["Line32", "Line33", "Line34"],
-    "v18": ["Line33", "Line50", "Line51"],
-    "v19": ["Line51", "Line52", "Line55"],
-    "v20": ["Line49", "Line50", "Line53"],
-    "v21": ["Line12", "Line35", "Line47"],
-    "v22": ["Line12", "Line36", "Line37"],
-    "v23": ["Line37", "Line46", "Line4"],
-    "v24": ["Line47", "Line48", "Line49"],
-    "v25": ["Line45", "Line48", "Line46"],
-    "v26": ["Line43", "Line44", "Line45"],
-    "v27": ["Line25", "Line26", "Line31"]
-};
-const edges: Obj<string[]> = {
-    "LineA": ["x" ,"v0"],
-    "LineB": ["v1", "v6"],
-    "Line0": ["x", "v2"],
-    "Line1": ["v2", "v7"],
-    "Line2": ["v2", "v1"],
-    "Line3": ["v7", "v8"],
-    "Line4": ["v10", "v23"],
-
-    "Line8": ["v0", "v1"],
-    "Line9": ["v4", "v5"],
-    "Line10": ["v3", "x"],
-
-    "Line12": ["v22", "v21"],
-
-    "Line15": ["v0", "v3"],
-    "Line16": ["x", "v14"],
-    "Line17": ["v14", "v6"],
-    "Line18": ["v14", "v9"],
-    "Line19": ["x", "v7"],
-    "Line20": ["x" ,"v8"],
-    "Line21": ["v8", "v9"],
-    "Line22": ["x", "v12"],
-    "Line23": ["v9", "v10"],
-    "Line24": ["v6", "v15"],
-    "Line25": ["v15", "v27"],
-    "Line26": ["v27", "v5"],
-    "Line27": ["v3", "v4"],
-    "Line28": ["v4", "x"],
-
-    "Line30": ["v5", "x"],
-    "Line31": ["v27", "v16"],
-    "Line32": ["v16", "v17"],
-    "Line33": ["v18", "v17"],
-    "Line34": ["v17", "x"],
-    "Line35": ["v16", "v21"],
-    "Line36": ["v15", "v22"],
-    "Line37": ["v22", "v23"],
-    "Line38": ["v10", "v11"],
-    "Line39": ["v12", "v11"],
-    "Line40": ["v12", "v13"],
-    "Line41": ["x", "v13"],
-    "Line42": ["v13", "x"],
-    "Line43": ["v11", "v26"],
-    "Line44": ["v26", "x"],
-    "Line45": ["v26", "v25"],
-    "Line46": ["v23", "v25"],
-    "Line47": ["v21", "v24"],
-    "Line48": ["v25", "v24"],
-    "Line49": ["v24", "v20"],
-    "Line50": ["v20", "v18"],
-    "Line51": ["v18", "v19"],
-    "Line52": ["x", "v19"],
-    "Line53": ["v20", "x"],
-
-    "Line55": ["v19", "x"],
-};
 /**
  * Trace a circle around the zig-zaggy track of a kri8it painting
  */
 export function krii8it($section: Step) {
+    // hardcoded data... could it be simpler?
+    const vertices: Obj<string[]> = {
+        "x": ["LineA", "Line0", "Line10", "Line16", "Line19", "Line22", "Line28", "Line30",
+    "Line34", "Line41", "Line42", "Line44", "Line52", "Line55"],
+        "v0": ["LineA", "Line8", "Line15"],
+        "v1": ["Line8", "LineB", "Line2"],
+        "v2": ["Line0", "Line2", "Line1"],
+        "v3": ["Line10", "Line15", "Line27"],
+        "v4": ["Line27", "Line28", "Line9"],
+        "v5": ["Line26", "Line9", "Line30"],
+        "v6": ["LineB", "Line17", "Line24"],
+        "v7": ["Line1", "Line19", "Line3"],
+        "v8": ["Line3", "Line20", "Line21"],
+        "v9": ["Line21", "Line18", "Line23"],
+        "v10": ["Line23", "Line38", "Line4"],
+        "v11": ["Line38", "Line39", "Line43"],
+        "v12": ["Line39", "Line40", "Line22"],
+        "v13": ["Line40", "Line41", "Line42"],
+        "v14": ["Line16", "Line17", "Line18"],
+        "v15": ["Line24", "Line25", "Line36"],
+        "v16": ["Line31", "Line32", "Line35"],
+        "v17": ["Line32", "Line33", "Line34"],
+        "v18": ["Line33", "Line50", "Line51"],
+        "v19": ["Line51", "Line52", "Line55"],
+        "v20": ["Line49", "Line50", "Line53"],
+        "v21": ["Line12", "Line35", "Line47"],
+        "v22": ["Line12", "Line36", "Line37"],
+        "v23": ["Line37", "Line46", "Line4"],
+        "v24": ["Line47", "Line48", "Line49"],
+        "v25": ["Line45", "Line48", "Line46"],
+        "v26": ["Line43", "Line44", "Line45"],
+        "v27": ["Line25", "Line26", "Line31"]
+    };
+    const edges: Obj<string[]> = {
+        "LineA": ["x" ,"v0"],
+        "LineB": ["v1", "v6"],
+        "Line0": ["x", "v2"],
+        "Line1": ["v2", "v7"],
+        "Line2": ["v2", "v1"],
+        "Line3": ["v7", "v8"],
+        "Line4": ["v10", "v23"],
+
+        "Line8": ["v0", "v1"],
+        "Line9": ["v4", "v5"],
+        "Line10": ["v3", "x"],
+
+        "Line12": ["v22", "v21"],
+
+        "Line15": ["v0", "v3"],
+        "Line16": ["x", "v14"],
+        "Line17": ["v14", "v6"],
+        "Line18": ["v14", "v9"],
+        "Line19": ["x", "v7"],
+        "Line20": ["x" ,"v8"],
+        "Line21": ["v8", "v9"],
+        "Line22": ["x", "v12"],
+        "Line23": ["v9", "v10"],
+        "Line24": ["v6", "v15"],
+        "Line25": ["v15", "v27"],
+        "Line26": ["v27", "v5"],
+        "Line27": ["v3", "v4"],
+        "Line28": ["v4", "x"],
+
+        "Line30": ["v5", "x"],
+        "Line31": ["v27", "v16"],
+        "Line32": ["v16", "v17"],
+        "Line33": ["v18", "v17"],
+        "Line34": ["v17", "x"],
+        "Line35": ["v16", "v21"],
+        "Line36": ["v15", "v22"],
+        "Line37": ["v22", "v23"],
+        "Line38": ["v10", "v11"],
+        "Line39": ["v12", "v11"],
+        "Line40": ["v12", "v13"],
+        "Line41": ["x", "v13"],
+        "Line42": ["v13", "x"],
+        "Line43": ["v11", "v26"],
+        "Line44": ["v26", "x"],
+        "Line45": ["v26", "v25"],
+        "Line46": ["v23", "v25"],
+        "Line47": ["v21", "v24"],
+        "Line48": ["v25", "v24"],
+        "Line49": ["v24", "v20"],
+        "Line50": ["v20", "v18"],
+        "Line51": ["v18", "v19"],
+        "Line52": ["x", "v19"],
+        "Line53": ["v20", "x"],
+
+        "Line55": ["v19", "x"],
+    };
     // select all the [lines, paths, polylines] | *path from kri8it
     
     const $lines = $section.$$('path, line, polyline') as SVGView[];
     const $runner = $section.$('#runner') as SVGView;
 
     // for figuring out which is which (might be easier to just do this in Sketch)
-    $lines.forEach(l => l.on('click', () => console.log(l.id)));
+    // $lines.forEach(l => l.on('click', () => console.log(l.id)));
 
     const velocityInverse = 3;
     const durations = $lines.map(l => l.strokeLength * velocityInverse);
 
     let a2 = (edgeKey: string, reverse: boolean): AnimationResponse => {
         let edge = $lines.find(l => l.id === edgeKey)!;
-        console.log("Found edge:")
-        console.log(edge);
 
         return animate(function(p) {
             // consider which direction to send the runner
@@ -805,8 +746,7 @@ export function krii8it($section: Step) {
         let choices = vertices[v].filter(ee => ee != e);
         let choiceEdge = choices[Math.floor(Math.random() * choices.length)];
         let edgeDirections = edges[choiceEdge];
-        console.log(choices);
-        console.log(choiceEdge);
+
         a2(choiceEdge, edgeDirections.indexOf(v) === 1).promise.then(() => {
             let nextVertices = edges[choiceEdge];
             let next = nextVertices.find(vx => vx !== v);
@@ -820,28 +760,11 @@ export function krii8it($section: Step) {
     $section.$('.button')?.on('click', () => {
         recurse2("x");
     });
-
-    return;
-    let a = (i: number): AnimationResponse => {
-        return animate(function(p) {
-            let point = $lines[i].getPointAt(p);
-            let translated = point.translate(new Point(14.620133, 14)); // shift by the "translate" property of the root
-            $runner.setCenter(translated);
-        }, durations[i]);
-    }
-
-    // make it recursive instead of hard-coding
-    let recurse = (i: number, N: number) => {
-        // console.log(`i = ${i}; N = ${N}`);
-        if (i >= N) return;
-        a(i).promise.then(() => {
-            recurse(++i, N);
-        });
-    }
-
-    recurse(0, $lines.length);
 }
 
+/**
+ * A quiz for testing parity bits
+ */
 export function hamming($step: Step) {
 
   const $gameplay = $step.$('x-gameplay') as Gameplay;
