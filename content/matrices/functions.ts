c325fe100c112818f40e9b707f7f4b653b3151a6
Kevin DeLand <kevin.deland@gmail.com>
2020-08-07 12:07:41 -0400

    fixed indents and lint

--- content/matrices/functions.ts
+++ content/matrices/functions.ts
@@ -3,9 +3,9 @@
 // (c) Mathigon
 // =============================================================================
 
-import { Step } from "../shared/types";
-import { ElementView } from "@mathigon/boost";
-import { Point } from "@mathigon/fermat";
+import {Step} from '../shared/types';
+import {ElementView} from '@mathigon/boost';
+import {Point} from '@mathigon/fermat';
 
 /**
  *
@@ -14,10 +14,10 @@ import { Point } from "@mathigon/fermat";
  */
 function applyTransform(A: number[][], v: number[]): number[] {
 
-    return [
-        A[0][0]*v[0] + A[0][1]*v[1],
-        A[1][0]*v[0] + A[1][1]*v[1]
-    ]
+  return [
+    A[0][0]*v[0] + A[0][1]*v[1],
+    A[1][0]*v[0] + A[1][1]*v[1]
+  ];
 }
 
 /**
@@ -27,20 +27,20 @@ function applyTransform(A: number[][], v: number[]): number[] {
  */
 export function translations($step: Step) {
 
-    const $polygons = $step.$('svg')?.$$('polygon') as ElementView[];
-    const origin = {x: -40, y: -30}; // center polygon on "origin" (top left)
-    const center = {x : 110, y: 110};
-    const cascade = {x: origin.x + center.x, y: origin.y + center.y};
-  
-    $polygons[0].setTransform(cascade);
-    $polygons[1].setTransform(cascade);
-    
-    $step.model.watch((state: any) => {
-      console.log(state.a);
-  
-      const scale = {x: cascade.x * state.a, y: cascade.y * 1};
-      $polygons[1].setTransform(cascade, 0, state.a);
-    });
+  const $polygons = $step.$('svg')?.$$('polygon') as ElementView[];
+  const origin = {x: -40, y: -30}; // center polygon on "origin" (top left)
+  const center = {x: 110, y: 110};
+  const cascade = {x: origin.x + center.x, y: origin.y + center.y};
+
+  $polygons[0].setTransform(cascade);
+  $polygons[1].setTransform(cascade);
+
+  $step.model.watch((state: any) => {
+    console.log(state.a);
+
+    const _scale = {x: cascade.x * state.a, y: cascade.y * 1};
+    $polygons[1].setTransform(cascade, 0, state.a);
+  });
 }
 
 /**
@@ -48,160 +48,160 @@ export function translations($step: Step) {
  * @param $step
  */
 export function rotations($step: Step) {
-    
-    const $polygons = $step.$('svg')?.$$('polygon') as ElementView[];
-    const center = {x: 50, y: 50}
-    console.log($polygons[1]);
-  
-    $step.model.watch((state: any) => {
-      console.log(state.angle);
-      // yeah, so this isn't doing what I want!
-      $polygons[1].setTransform(center, Math.PI * state.angle / 360);
-    });
+
+  const $polygons = $step.$('svg')?.$$('polygon') as ElementView[];
+  const center = {x: 50, y: 50};
+  console.log($polygons[1]);
+
+  $step.model.watch((state: any) => {
+    console.log(state.angle);
+    // yeah, so this isn't doing what I want!
+    $polygons[1].setTransform(center, Math.PI * state.angle / 360);
+  });
 }
-  
+
 /**
  * Very cool.
  * NEXT: center on theorigin and make it reflect about
  * NEXT: make the origin thicker
- * 
+ *
  * @param $step
  */
 export function scale($step: Step) {
-  
-    $step.model.polygonScale = (xscale: number, yscale: number) => {
-      console.log(xscale, yscale);
-  
-      // here's where we have to do that p5js strategy where we push and pop translates to display it
-      // (1) center polygon along the origin (0,0)
-      // (2) apply transformation shown in matrix
-      // (3) move it to center (110, 110)
 
-      const points = [[30,10], [10,70], [70,70], [50,10]];
+  $step.model.polygonScale = (xscale: number, yscale: number) => {
+    console.log(xscale, yscale);
 
-      // let's try some d3 style formatting
-      const pointString = points.map(p => [p[0]-40, p[1]-40])         // (1) center shape along origin (0,0)
-                                .map(p => applyTransform([            // (2) apply transformation from matrix
-                                    [xscale, 0],
-                                    [0, yscale]
-                                ], p))
-                                .map(p => [p[0]+110, [p[1]+110]])     // (3) move to center of SVG
-                                .map(point => point.join(','))        // commas between xy coords
-                                .join(' ')                            // spaces between coord pairs
-
-      const poly = `<polygon points="${pointString}" fill="#ff941f" opacity="0.5" />`;
-  
-      return poly;
-    }
-}
-
-/**
- * Also very cool.
- * 
- * @param $step 
- */
-export function skew($step: Step) {
-  
     // here's where we have to do that p5js strategy where we push and pop translates to display it
     // (1) center polygon along the origin (0,0)
     // (2) apply transformation shown in matrix
     // (3) move it to center (110, 110)
 
-    $step.model.polygonSkew = (xskew: number, yskew: number) => {
-      console.log(xskew, yskew);
-  
-      const points = [[30,10], [10,70], [70,70], [50,10]];
+    const points = [[30, 10], [10, 70], [70, 70], [50, 10]];
 
-      // let's try some d3 style formatting
-      const pointString = points.map(p => [p[0]-40, p[1]-40])         // (1) center shape along origin (0,0)
-                                .map(p => applyTransform([
-                                    [1, - xskew],  // why is this negative?
-                                    [-yskew, 1]   // is this the right direction?
-                                ], p)) // (2) apply transformation from matrix
-                                .map(p => [p[0]+110, [p[1]+110]])     // (3) move to center of SVG
-                                .map(point => point.join(','))        // commas between xy coords
-                                .join(' ')                            // spaces between coord pairs
+    // let's try some d3 style formatting
+    const pointString = points.map(p => [p[0]-40, p[1]-40])         // (1) center shape along origin (0,0)
+        .map(p => applyTransform([            // (2) apply transformation from matrix
+          [xscale, 0],
+          [0, yscale]
+        ], p))
+        .map(p => [p[0]+110, [p[1]+110]])     // (3) move to center of SVG
+        .map(point => point.join(','))        // commas between xy coords
+        .join(' ');                            // spaces between coord pairs
 
-      const poly = `<polygon points="${pointString}" fill="#ff941f" opacity="0.5" />`;
-  
-      return poly;
-    }
-  
-    console.log('inside of skew');
-    console.log('here is the model')
-    console.log($step.model);
+    const poly = `<polygon points="${pointString}" fill="#ff941f" opacity="0.5" />`;
+
+    return poly;
+  };
+}
+
+/**
+ * Also very cool.
+ *
+ * @param $step
+ */
+export function skew($step: Step) {
+
+  // here's where we have to do that p5js strategy where we push and pop translates to display it
+  // (1) center polygon along the origin (0,0)
+  // (2) apply transformation shown in matrix
+  // (3) move it to center (110, 110)
+
+  $step.model.polygonSkew = (xskew: number, yskew: number) => {
+    console.log(xskew, yskew);
+
+    const points = [[30, 10], [10, 70], [70, 70], [50, 10]];
+
+    // let's try some d3 style formatting
+    const pointString = points.map(p => [p[0]-40, p[1]-40])         // (1) center shape along origin (0,0)
+        .map(p => applyTransform([
+          [1, - xskew],  // why is this negative?
+          [-yskew, 1]   // is this the right direction?
+        ], p)) // (2) apply transformation from matrix
+        .map(p => [p[0]+110, [p[1]+110]])     // (3) move to center of SVG
+        .map(point => point.join(','))        // commas between xy coords
+        .join(' ');                            // spaces between coord pairs
+
+    const poly = `<polygon points="${pointString}" fill="#ff941f" opacity="0.5" />`;
+
+    return poly;
+  };
+
+  console.log('inside of skew');
+  console.log('here is the model');
+  console.log($step.model);
 }
 
 // NEXT: figure out why the heck this ain't workin'
 export function rotate($step: Step) {
-  
-    $step.model.polygonRotate = (angle: number) => {
-      console.log('rotate', angle);
-  
-      const rads = Math.PI * angle / 360;
-      const cos = Math.cos(rads);
-      const sin = Math.sin(rads);
-  
-      const points = [[30,10], [10,70], [70,70], [50,10]];
 
-      // let's try some d3 style formatting
-      const pointString = points.map(p => [p[0]-40, p[1]-40])         // (1) center shape along origin (0,0)
-                                .map(p => applyTransform([ // apply transformation from matrix
-                                    [cos, -sin], // why is this negative?
-                                    [sin, cos] // is this the right direction?
-                                ], p))
-                                .map(p => [p[0]+110, [p[1]+110]])     // (3) move to center of SVG
-                                .map(point => point.join(','))        // commas between xy coords
-                                .join(' ')                            // spaces between coord pairs
+  $step.model.polygonRotate = (angle: number) => {
+    console.log('rotate', angle);
 
-      /*const transformedPoints = points.map(p =>
+    const rads = Math.PI * angle / 360;
+    const cos = Math.cos(rads);
+    const sin = Math.sin(rads);
+
+    const points = [[30, 10], [10, 70], [70, 70], [50, 10]];
+
+    // let's try some d3 style formatting
+    const pointString = points.map(p => [p[0]-40, p[1]-40])         // (1) center shape along origin (0,0)
+        .map(p => applyTransform([ // apply transformation from matrix
+          [cos, -sin], // why is this negative?
+          [sin, cos] // is this the right direction?
+        ], p))
+        .map(p => [p[0]+110, [p[1]+110]])     // (3) move to center of SVG
+        .map(point => point.join(','))        // commas between xy coords
+        .join(' ');                            // spaces between coord pairs
+
+    /* const transformedPoints = points.map(p =>
         [cos * p[0] - sin * p[1],
         sin * p[0] + cos * p[1]]
       );
       const pointString = transformedPoints.map(point => + point[0] + "," + point[1]).join(' ');*/
-      const poly0 = `<polygon points="${pointString}" fill="#ff941f" opacity="0.5" />`;
-  
-      return poly0;
-    }
-  
-    console.log('inside of rotate');
-    console.log('here is the model')
-    console.log($step.model);
-  
+    const poly0 = `<polygon points="${pointString}" fill="#ff941f" opacity="0.5" />`;
+
+    return poly0;
+  };
+
+  console.log('inside of rotate');
+  console.log('here is the model');
+  console.log($step.model);
+
 }
 
 export function playWithMe($step: Step) {
 
-    console.log($step.model)
+  console.log($step.model);
 
-    // buttons
-    const buttons = $step.$$('.button');
+  // buttons
+  const buttons = $step.$$('.button');
 
-    // paths
-    const fabric = $step.$$('.fabric');
+  // paths
+  const _fabric = $step.$$('.fabric');
 
-    // QUESTION: how to animate these instead of just setting them instantly?
-    // identity
-    buttons[0].on('click', () => {
-        $step.model.ipoint = new Point(1, 0)
-        $step.model.jpoint = new Point(0, 1)
-    });
+  // QUESTION: how to animate these instead of just setting them instantly?
+  // identity
+  buttons[0].on('click', () => {
+    $step.model.ipoint = new Point(1, 0);
+    $step.model.jpoint = new Point(0, 1);
+  });
 
-    // skew
-    buttons[1].on('click', () => {
-        $step.model.ipoint = new Point(1, 0)
-        $step.model.jpoint = new Point(1, 1)
-    });
+  // skew
+  buttons[1].on('click', () => {
+    $step.model.ipoint = new Point(1, 0);
+    $step.model.jpoint = new Point(1, 1);
+  });
 
-    // scale
-    buttons[2].on('click', () => {
-        $step.model.ipoint = new Point(2, 0)
-        $step.model.jpoint = new Point(0, 2)
-    });
+  // scale
+  buttons[2].on('click', () => {
+    $step.model.ipoint = new Point(2, 0);
+    $step.model.jpoint = new Point(0, 2);
+  });
 
-    // rotate
-    buttons[3].on('click', () => {
-        $step.model.ipoint = new Point(0, 1)
-        $step.model.jpoint = new Point(-1, 0)
-    });
+  // rotate
+  buttons[3].on('click', () => {
+    $step.model.ipoint = new Point(0, 1);
+    $step.model.jpoint = new Point(-1, 0);
+  });
 }
