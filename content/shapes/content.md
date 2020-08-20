7d07a8259fd928a252aad6a26883ca888de82900
Patrick J. Henning <path@fea.st>
2020-08-19 17:18:59 -0700

    Drafting voronoi diagram exercise

--- content/shapes/content.md
+++ content/shapes/content.md
@@ -23,20 +23,32 @@ TODO
 > section: polygons
 > sectionStatus: dev
 > id: voronoi
-> goal: move-points
+> goals: one-point five-points voronoi-diagram eight-points
 
 School districts  need an accurate estimate of the number of students that may attend their schools. Below is a map of primary schools in London. In general, students attend the school closest to where they live. Click anywhere on the map to see the distance between that point and all the schools on the map.
 
     figure: x-geopad(width=600 height=400)
       canvas.voronoi(width=600 height=400)
       svg
-        circle.move(cx=40 cy=60 name="a")
-        circle.move(cx=60 cy=80 name="b")
-        circle.move(cx=80 cy=100 name="c")
+        circle.red(cx=40 cy=60 name="a")
+        circle.red(cx=500 cy=80 name="b")
+        circle.red(cx=170 cy=130 name="c")
+        circle.red(cx=228 cy=240 name="d")
+        circle.red(cx=400 cy=180 name="e")
+        circle.red(cx=300 cy=330 name="f")
+        circle.red(cx=320 cy=138 name="g")
+        circle.red(cx=260 cy=40 name="h")
 
----
+{.reveal(when="one-point")} Pick 4 other points on the map.
 
-The next step
+{.reveal(when="five-points")} _{button.btn.show-voronoi} Create voronoi diagram_
+
+{.reveal(when="voronoi-diagram")} We’ve just made a Voronoi diagram. Each region contains all the points on the map that are closest to the school in that region. Click a few spots in different regions to check. 
+
+{.reveal(when="eight-points")} The first person to think about these things was probably [Rene Descartes](https://mathigon.org/timeline/descartes) 400 years ago - he was a famous philosopher too!  But they’re named after a Russian mathematician, [Gregory Feodosevich Voronoy](https://en.wikipedia.org/wiki/Georgy_Voronoy). Physician John Snow used a version of a Voronoi diagram in 1854 during a [cholera epidemic](https://plus.maths.org/content/uncovering-cause-cholera) In London to locate an infected water pump on Broad Street in London 
+
+{.reveal(when="eight-points")} In our example, using a Voronoi diagram can help schools make estimates of how many students will attend. In London, about 1500 people live in each square kilometer. To use this statistic, we first need to first know the [[area | perimeter]] of each region in the diagram. 
+
 
 ---
 
--- content/shapes/functions.ts
+++ content/shapes/functions.ts
@@ -4,17 +4,140 @@
 // =============================================================================
 
 
-import {Polygon} from '@mathigon/fermat';
-import {CanvasView} from '@mathigon/boost';
-import {Step} from '../shared/types';
+import {Polygon, Point, Segment} from '@mathigon/fermat';
+import {CanvasView, loadScript, animate} from '@mathigon/boost';
+import {Step, Geopad, GeoPoint} from '../shared/types';
 
+declare const d3: any;
 
-export function voronoi($step: Step) {
+export async function voronoi($step: Step) {
+
+  await loadScript('/resources/shared/vendor/d3-delaunay.min.js');
+
   const $canvas = $step.$('canvas.voronoi') as CanvasView;
+  const $geopad = $step.$('x-geopad') as Geopad;
+  const $voronoiButton = $step.$('button.show-voronoi')!;
+  const bounds = [0, 0, $canvas.canvasWidth, $canvas.canvasHeight];
+
+  $step.model.dynPoints = [];
+  $step.model.distLines = [];
+  $step.model.vorOpacity = 0;
+
+  const cafePoints: Point[] = [
+    $step.model.a, $step.model.b, $step.model.c, $step.model.d, 
+    $step.model.e, $step.model.f, $step.model.g, $step.model.h
+  ];
+
+  const dt = d3.Delaunay.from(cafePoints, getX, getY);
+  const vor = dt.voronoi(bounds);
+
+  const cellsRaw: number[][][] = Array.from(vor.cellPolygons());
+
+  const cellPolys =
+    cellsRaw.map(cellRaw => {
+      const cellPoints = cellRaw.map(pointRaw => {
+        return new Point(...pointRaw);
+      });
+      return new Polygon(...cellPoints);
+    });
+  
+  $voronoiButton.on('click', _ => {
+    showVor($step);
+  });
+
+  $geopad.switchTool('point');
+  $geopad.on('add:point', ({point}: {point: GeoPoint}) => {
+
+    point.lock();
+
+    const p = point.value!;
+    const edges: Segment[] = [];
+
+    let shortest = {len: Number.POSITIVE_INFINITY, ind: 0};
+    cafePoints.forEach((cafePoint, i) => {
+      const newEdge = new Segment(cafePoint, p);
+      if (newEdge.length < shortest.len) {
+        shortest.len = newEdge.length;
+        shortest.ind = i;
+      }
+      edges.push(newEdge);
+    });
+
+    const preLen = $step.model.distLines.length;
+
+    edges.forEach((edge, i) => {
+      const stroke = i == shortest.ind ? 'red' : 'black';
+      $step.model.distLines.push({edge, stroke, opacity: 1});
+      $step.model.distLines = $step.model.distLines.slice();
+      if (i != shortest.ind) {
+        handleAnim(i + preLen, $step);
+      }
+    });
+
+    $step.model.dynPoints.push(p);
+    $step.model.dynPoints = $step.model.dynPoints.slice();
+
+  });
 
   $step.model.watch(() => {
+
+    if ($step.model.dynPoints.length == 1 && !$step.model.promptMorePoints) {
+      $step.score('one-point');
+      $step.model.promptMorePoints == true;
+    }
+
+    if ($step.model.dynPoints.length == 5 && !$step.model.showButton) {
+      $step.score('five-points');
+      // TODO show button
+      $step.model.showButton = true;
+    }
+
+    if ($step.model.dynPoints.length == 8 && !$step.model.eightPoints) {
+      $step.score('eight-points');
+      $step.model.eightPoints = true;
+    }
+
     $canvas.clear();
-    const poly = new Polygon($step.model.a, $step.model.b, $step.model.c);
-    $canvas.draw(poly, {fill: '#c00'});
+
+    if ($step.model.vorOpacity != 0) {
+      cellPolys.forEach(cellPoly => {
+        $canvas.draw(cellPoly, {stroke: 'black', strokeWidth: 2, opacity: $step.model.vorOpacity});
+      });
+    }
+
+    $step.model.distLines.forEach(({edge, stroke, opacity}: {edge: Segment, stroke: string, opacity: number}) => {
+      if (opacity != 0) {
+        $canvas.draw(edge, {strokeWidth: 2, stroke, opacity});
+      }
+    });
   });
+
+}
+
+async function handleAnim(index: number, $step: Step) {
+  window.setTimeout((_: any) => {
+    const anim = animate((progress, _) => {
+      let edges: {edge: Segment, stroke: string, opacity: number}[] = $step.model.distLines;
+      const opacity = 1 - progress;
+      let e = edges[index];
+      e.opacity = opacity;
+      edges[index] = e;
+      $step.model.distLines = edges.slice();
+    }, 500);
+  }, 1000);
+}
+
+async function showVor($step: Step) {
+  const anim = animate((progress, _) => {
+    $step.model.vorOpacity = progress;
+  }, 2000);
+  anim.promise.then(_ => $step.score('voronoi-diagram'));
+}
+
+function getX(p: Point) {
+  return p.x;
+}
+
+function getY(p: Point) {
+  return p.y;
 }
