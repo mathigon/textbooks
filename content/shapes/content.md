ba2d4961890a20e24d93faf4d3151b0aa7d34e6a
Philipp Legner <philipp@mathigon.org>
2020-09-05 00:54:20 +0100

    Add tangram component to shapes course

--- content/shapes/content.md
+++ content/shapes/content.md
@@ -61,6 +61,15 @@ All of these regions in the Voronoi diagram are examples of polygons. A polygon 
 {.reveal(when="cards-sorted")} A regular polygon has [[all equal | all different]] side lengths. 
 
 ---
+> id: tangram
+
+Here is a Tangram canvas:
+
+    figure: .tangram
+      x-polypad
+      .shapes
+
+---
 
 ## Circles and Circumferences
 
--- content/shapes/functions.ts
+++ content/shapes/functions.ts
@@ -4,8 +4,9 @@
 // =============================================================================
 
 
+import {Point} from '@mathigon/fermat';
 import {BinarySwipe} from '../shared/components/binary-swipe';
-import {Step} from '../shared/types';
+import {Polypad, Step} from '../shared/types';
 import '../shared/components/binary-swipe';  // import component
 
 
@@ -19,5 +20,26 @@ export function sortPolygons($step: Step) {
   $sort.on('incorrect', ({hint}) => $step.addHint(hint, {class: 'incorrect'}));
   $sort.on('complete', () => {
     console.log('Time for next step');
+  });
+}
+
+export function tangram($step: Step) {
+  const $polypad = $step.$('x-polypad') as Polypad;
+  $polypad.$svg.setAttr('viewBox', '0 0 420 420');
+  $polypad.canDelete = $polypad.canCopy = false;
+
+  const tiles = [[2, 4, -90], [4, 2, 0], [6, 4, 0], [3, 7, 0], [7, 2, -90],
+    [4, 5, 0], [6, 6, 90]];  // initial [x, y, rot]
+
+  for (const [i, t] of tiles.entries()) {
+    const tile = $polypad.newTile('tangram', `${i}`);
+    tile.setPosition(new Point(110 + t[0] * 25, 110 + t[1] * 25));
+    tile.setRotation(t[2]);
+  }
+
+  // TODO Allow students to select different silhouettes
+
+  $polypad.on('move-selection rotate-selection', () => {
+    // TODO Check if a shape has been completed
   });
 }
