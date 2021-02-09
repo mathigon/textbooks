e79fabc1d2f95c89ecb37642d59a2c096ebd0c61
Christopher Walker <cwalker@polytrope.com>
2021-02-09 12:58:32 -0500

    Complete relation plotting interactive
    Incorrect points are now removed immediately. Points are locked to prevent dragging after placement. A little burst animation plays when points are placed; this is partly for fun, but also to emphasize the that action is happening to touchscreen users whose fingers may be covering the point upon its placement.

--- content/functions/functions.ts
+++ content/functions/functions.ts
@@ -5,8 +5,10 @@
 
 
 import '../shared/components/relation';
-import {Point} from '@mathigon/euclid';
+import {Point, SimplePoint} from '@mathigon/euclid';
 import {Geopad, GeoPoint, Step} from '../shared/types';
+import { $N, animate, ease, SVGParentView } from '@mathigon/boost';
+import { Burst } from '../shared/components/burst';
 
 
 export function fnSketch($step: Step) {
@@ -21,19 +23,37 @@ export function coordinatePlots($step: Step) {
 
   const targets = [new Point(-5, 3), new Point(-2, -1), new Point(4, -3)];
 
-  $geopad.on('add:point moveEnd', (e) => {
+  $geopad.on('add:point', (e) => {
     const point = (e.point || e.path) as GeoPoint;  // Messy, sorry!
     const index = targets.findIndex(p => point.value?.equals(p));
+
+    var burstType: string;
 
     if (index < 0) {
       $step.addHint('incorrect');
       point.$el.addClass('red');
+      point.lock();
+      burstType = 'burst incorrect';
+
+      animate((p) => {
+        const q = Math.sqrt(ease('bounce-in', 1-p));
+        point.$el.css('transform', `scale(${q})`);
+      }, 1000).promise.then(() => {
+        point.delete();
+      });
     } else {
       $step.score('p' + index);
       $step.addHint('correct');
-      point.lock();
       point.$el.addClass('green');
+      point.lock();
+      burstType = 'burst correct';
     }
+      
+    const burstElement = $N('g', {class: burstType}, $geopad.$svg);
+    const burst = new Burst(burstElement as SVGParentView, 10);
+    burst.play(1000, [point.$el.center.x, point.$el.center.y], [5, 25]).then(() => {
+      burstElement.remove();
+    })
   });
 
   $step.onScore('p0 p1 p2', () => {
