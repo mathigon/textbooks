300a2c7b62a24189587a350202e3bcef944eb212
Patrick J. Henning <path@fea.st>
2021-03-22 00:43:39 -0700

    Merge droppable features into Draggable class

--- content/shared/components/droppable.ts
+++ content/shared/components/droppable.ts
@@ -2,6 +2,11 @@ import {$html, Browser, ElementView, slide, SVGParentView} from '@mathigon/boost
 import {applyDefaults, EventTarget} from '@mathigon/core';
 import {Bounds, Point} from '@mathigon/euclid';
 
+type HoverData =
+  {tag: 'Target', $el: ElementView} |
+  {tag: 'NonTarget'};
+
+
 interface DraggableOptions {
   moveX?: boolean;  // Whether it is draggable along the x-axis.
   moveY?: boolean;  // Whether it is draggable along the y-axis.
@@ -11,19 +16,27 @@ interface DraggableOptions {
   round?: ((p: Point) => Point);  // Custom rounding function.
   width?: number;  //  Override `$parent` width.
   height?: number;  // Override `$parent` height.
+  resetOnMiss?: boolean;
+  $targets?: ElementView[];
 }
 
 /**
- * A draggable HTML element.
+ * A draggable and droppable HTML element.
  * @emits {void} Draggable#start when the user starts dragging this element.
  * @emits {[Point, Point]} ($el position, pointer position) Draggable#drag while the user is dragging this element.
  * @emits {void} Draggable#click when the user clicks on the this element.
  * @emits {void} Draggable#end after the user stops dragging this element.
  * @emits {Point} Draggable#move When the position of this element changes.
+ * @emits Draggable#enter-target {ElementView} $target - Fires when the pointer has entered the bounds of a 'target' element while dragging
+ * @emits Draggable#exit-target {ElementView} $target - Fires when the pointer has exited the bounds of a 'target' element while dragging
+ * @emits Draggable#dropped-target {ElementView} $target - Fires when the user releases the pointer while over a 'target' element
+ * @emits Draggable#dropped-non-target - Fires when the user releases the pointer while not over a 'target' element
  */
-export class DropDraggable extends EventTarget {
+export class Draggable extends EventTarget {
   protected options: DraggableOptions;
   protected areaBounds = {topLeft: new Point(0, 0), bottomRight: new Point(0, 0)};
+  protected over: HoverData;
+  protected startPos = new Point(0, 0);
   position = new Point(0, 0);
   disabled = false;
   width = 0;
@@ -33,27 +46,89 @@ export class DropDraggable extends EventTarget {
       options: DraggableOptions = {}) {
     super();
 
+    this.over = {tag: 'NonTarget'};
+
     this.options = applyDefaults(options, {moveX: true, moveY: true});
     this.setDimensions($parent);
 
-    let startPosn: Point;
     slide($el, {
       start: () => {
         if (this.disabled) return;
-        startPosn = this.position;
+        this.startPos = this.position;
         this.trigger('start');
         $html.addClass('grabbing');
       },
       move: (posn, start) => {
         if (this.disabled) return;
-        this.setPosition(startPosn!.x + posn.x - start.x,
-                         startPosn!.y + posn.y - start.y);
-        this.trigger('drag', [this.position, posn]); // pass obj instead of tuple
+        this.setPosition(
+            this.startPos.x + posn.x - start.x,
+            this.startPos.y + posn.y - start.y
+        );
+        this.trigger('drag', {elemPos: this.position, pointerPos: posn}); // pass obj instead of tuple
+
+        let overTarget = false;
+        const prevTarget = this.over.tag == 'NonTarget' ? undefined : this.over.$el;
+        if (this.options.$targets != undefined) {
+          for (const $target of this.options.$targets) {
+            if ($target.boundsRect.contains(posn)) {
+              overTarget = true;
+              if (prevTarget != $target) {
+                /**
+                 * Fires when the pointer has entered the bounds of a 'target' element while dragging
+                 *
+                 * @event Draggable#enter-target
+                 * @type {ElementView} $target - The element whose bounds the pointer has entered
+                 */
+                this.trigger('enter-target', $target);
+                this.over = {tag: 'Target', $el: $target};
+              }
+              break;
+            }
+          }
+
+          if (!overTarget) this.over = {tag: 'NonTarget'};
+          const currentTarget = this.over.tag == 'NonTarget' ? undefined : this.over.$el;
+          /**
+           * Fires when the pointer has exited the bounds of a 'target' element while dragging
+           *
+           * @event Draggable#exit-target
+           * @type {ElementView} $target - The element whose bounds the pointer has exited
+           */
+          if (prevTarget != undefined && prevTarget != currentTarget) this.trigger('exit-target', prevTarget);
+        }
       },
       end: (last, start) => {
         if (this.disabled) return;
         this.trigger(last.equals(start) ? 'click' : 'end');
         $html.removeClass('grabbing');
+
+        let droppedOn: ElementView | undefined;
+
+        if (this.options.$targets != undefined) {
+          for (const $target of this.options.$targets) {
+            if ($target.boundsRect.contains(last)) {
+              /**
+               * Fires when the user releases the pointer while over a 'target' element
+               *
+               * @event Draggable#dropped-target
+               * @type {ElementView} $target - The element the pointer was released over
+               */
+              this.trigger('dropped-target', $target);
+              droppedOn = $target;
+              break;
+            }
+          }
+        }
+
+        if (droppedOn == undefined) {
+          /**
+           * Fires when the user releases the pointer while not over a 'target' element
+           *
+           * @event Draggable#dropped-target
+           */
+          this.trigger('dropped-non-target');
+          if (this.options.resetOnMiss === true) this.resetPosition();
+        }
       },
       accessible: true
     });
@@ -112,116 +187,6 @@ export class DropDraggable extends EventTarget {
     }
 
     this.trigger('move', p);
-  }
-}
-
-interface DroppableOptions extends DraggableOptions {
-  resetOnMiss?: boolean;
-  $targets?: ElementView[];
-}
-
-type HoverData =
-  {tag: 'Target', $el: ElementView} |
-  {tag: 'NonTarget'};
-
-/**
- * A draggable and Droppable HTML element.
- * @emits Droppable#enter-target {ElementView} $target - Fires when the pointer has entered the bounds of a 'target' element while dragging
- * @emits Droppable#exit-target {ElementView} $target - Fires when the pointer has exited the bounds of a 'target' element while dragging
- * @emits Droppable#dropped-target {ElementView} $target - Fires when the user releases the pointer while over a 'target' element
- * @emits Draggable#dropped-non-target - Fires when the user releases the pointer while not over a 'target' element
- */
-export class Droppable extends DropDraggable {
-  protected options!: DroppableOptions
-  private pointerPos = new Point(0, 0);
-  private startPos = new Point(0, 0);
-  private over: HoverData;
-
-  constructor(
-      $el: ElementView,
-      $parent: ElementView,
-      options: DroppableOptions = {}
-  ) {
-    super($el, $parent, options);
-
-    this.options =
-      applyDefaults(this.options, {
-        resetOnMiss: true
-      });
-
-    this.over = {tag: 'NonTarget'};
-
-    this.on('start', async () => {
-      this.startPos = this.position;
-    });
-
-    this.on('drag', async ([_$elPos, pointerPos]: [Point, Point]) => {
-      this.pointerPos = pointerPos;
-      let overTarget = false;
-      const prevTarget = this.over.tag == 'NonTarget' ? undefined : this.over.$el;
-
-      if (this.options.$targets != undefined) {
-        for (const $target of this.options.$targets) {
-          if ($target.boundsRect.contains(pointerPos)) {
-            overTarget = true;
-            if (prevTarget != $target) {
-              /**
-               * Fires when the pointer has entered the bounds of a 'target' element while dragging
-               *
-               * @event Droppable#enter-target
-               * @type {ElementView} $target - The element whose bounds the pointer has entered
-               */
-              this.trigger('enter-target', $target);
-              this.over = {tag: 'Target', $el: $target};
-            }
-            break;
-          }
-        }
-
-        if (!overTarget) this.over = {tag: 'NonTarget'};
-        const currentTarget = this.over.tag == 'NonTarget' ? undefined : this.over.$el;
-        /**
-         * Fires when the pointer has exited the bounds of a 'target' element while dragging
-         *
-         * @event Droppable#exit-target
-         * @type {ElementView} $target - The element whose bounds the pointer has exited
-         */
-        if (prevTarget != undefined && prevTarget != currentTarget) this.trigger('exit-target', prevTarget);
-      }
-    });
-
-    this.on('end', async () => {
-
-      let droppedOn: ElementView | undefined;
-
-      if (this.options.$targets != undefined) {
-        for (const $target of this.options.$targets) {
-          if ($target.boundsRect.contains(this.pointerPos)) {
-            /**
-             * Fires when the user releases the pointer while over a 'target' element
-             *
-             * @event Droppable#dropped-target
-             * @type {ElementView} $target - The element the pointer was released over
-             */
-            this.trigger('dropped-target', $target);
-            droppedOn = $target;
-            break;
-          }
-        }
-      }
-
-      if (droppedOn == undefined) {
-        /**
-         * Fires when the user releases the pointer while not over a 'target' element
-         *
-         * @event Droppable#dropped-target
-         */
-        this.trigger('dropped-non-target');
-        if (this.options.resetOnMiss === true) this.resetPosition();
-      }
-
-    });
-
   }
 
   resetPosition() {
