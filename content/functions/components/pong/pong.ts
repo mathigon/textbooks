c0531123c86f2f6cc86557329e01640edac8985c
Christopher Walker <cwalker@polytrope.com>
2021-05-06 15:32:20 -0700

    Fill out pong functionality a bit

--- content/functions/components/pong/pong.ts
+++ content/functions/components/pong/pong.ts
@@ -11,7 +11,45 @@ import { clamp } from '@mathigon/fermat';
 
 @register('x-pong')
 export class Pong extends CustomElementView {
+    private $graph!: CoordinateSystem;
+    private $step?: Step;
+    private $paddle!: ElementView;
 
     ready() {
+        const $graph = this.$('x-coordinate-system') as CoordinateSystem;
+        this.$graph = $graph;
+
+        const paddleOriginPoint = new Point($graph.plotBounds.xMax, ($graph.plotBounds.yMax+$graph.plotBounds.yMin)/2);
+        const paddleOriginPosition = $graph.toViewportCoords(paddleOriginPoint);
+
+        const paddlePixelSize = $graph.viewportBounds.dy;
+        const paddlePixelRadius = paddlePixelSize/2;
+
+        const paddleMinPoint = new Point($graph.plotBounds.xMax, $graph.plotBounds.yMin);
+        const paddleMinPosition = $graph.toViewportCoords(paddleMinPoint);
+
+        const paddleMaxPoint = new Point($graph.plotBounds.xMax, $graph.plotBounds.yMax);
+        const paddleMaxPosition = $graph.toViewportCoords(paddleMaxPoint);
+
+        const $paddle = $N('g', {class: 'paddle', transform: `translate(${paddleOriginPosition.x},${paddleOriginPosition.y})`}, $graph.$overlay);
+        this.$paddle = $paddle;
+
+        const paddleLine = $N('line', {x1: 0, x2: 0, y1: -paddlePixelRadius, y2: paddlePixelRadius}, $paddle);
+
+        // const wall = $N('line')
+
+        slide($graph, {
+            down: () => {},
+            move: (position) => {
+                const y = clamp(position.y, paddleMinPosition.y+paddlePixelRadius, paddleMaxPosition.y-paddlePixelRadius);
+                $paddle.setAttr('transform', `translate(${paddleOriginPosition.x},${y})`);
+            },
+            up: () => {},
+            click: () => {},
+        });
+    }
+
+    bindStep($step: Step) {
+        this.$step = $step;
     }
 }
