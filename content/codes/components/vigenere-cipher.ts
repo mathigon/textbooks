7c112c75c4dfee8f2f0c6798d78b86134eab24b2
Seb Pearce <sebpearce@gmail.com>
2021-10-06 11:06:02 +1100

    Create input and key markers for vigenere square

--- content/codes/components/vigenere-cipher.ts
+++ content/codes/components/vigenere-cipher.ts
@@ -1,10 +1,13 @@
 // import {CustomElementView, ElementView, observe, register, SVGView} from '@mathigon/boost';
-import {CustomElementView, ElementView, register} from '@mathigon/boost';
+import {delay, wait} from '@mathigon/core';
+import {$, CustomElementView, ElementView, observe, register, SVGView} from '@mathigon/boost';
 import template from './vigenere-cipher.pug';
 
 @register('x-vigenere-cipher', {template})
 export class VigenereCipher extends CustomElementView {
   private $svg!: ElementView;
+  private $keyMarker!: ElementView;
+  private $inputMarker!: ElementView;
   //   private input!: string;
   //   private direction!: string;
   //   private targetLeftShift!: number;
@@ -15,15 +18,42 @@ export class VigenereCipher extends CustomElementView {
   //     currentShift: 0,
   //     lastShift: 0
   //   });
+  state = observe({
+    output: '',
+    curKey: '',
+    curInput: ''
+
+  });
 
   ready() {
-    const a = 'foo';
+    this.$svg = $('svg') as SVGView;
+    this.$keyMarker = $('.key-marker') as SVGView;
+    this.$inputMarker = $('.input-marker') as SVGView;
+
+    delay(() => {
+      this.setKeyMarkerPosition(0);
+      this.setInputMarkerPosition(15);
+    }, 500);
   }
 
   setShift() {
     // const lettersPerRadian = 26 / (Math.PI * 2);
     // const letterVal = Math.abs(lettersPerRadian * angle);
     // this.state.currentShift = Math.trunc(letterVal + 0.5) % 26;
+  }
+
+  setKeyMarkerPosition(n) {
+    this.$keyMarker.animate({transform: `translateY(${n * 20}px)`}, 750);
+  }
+
+  setInputMarkerPosition(n) {
+    this.$inputMarker.animate({transform: `translateX(${n * 20}px)`}, 750);
+  }
+
+  connectMarkers() {
+    // calculate output letter
+    // animate a line from the marker squares to the target letter square
+    // draw-animate target letter square
   }
 
   encode() {
